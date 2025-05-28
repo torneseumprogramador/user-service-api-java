f5a07578be8a77af15d8581f63c35e393557b6bc
Danilo Aparecido <danilo.aparecido.santos@gmail.com>
2025-05-28 19:57:06 -0300

    seeder

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user (name, email) VALUES ('Martins', 'martins@email.com');
+INSERT INTO user (name, email) VALUES ('Ana', 'ana@email.com');
+INSERT INTO user (name, email) VALUES ('Carlos', 'carlos@email.com');
